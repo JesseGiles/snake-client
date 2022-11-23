7a23c781209bfd130503cf5c70e06fe0f21d0f27
Jesse Giles <jesse.giles@telus.com>
2022-11-23 09:27:33 -0700

    moved connection function to client.js and now exporting to play.js

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //console log incoming data received from server
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  // client.on('data', (data) => {
+  //   console.log(data.toString());
+  //   client.end();
+  // })
+  
+  // client.on('end', () => {
+  //   console.log('disconnected from server');
+  // });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,30 +1,4 @@
-const net = require("net");
+//const net = require("net");
+const { connect } = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  // client.on('data', (data) => {
-  //   console.log(data.toString());
-  //   client.end();
-  // })
-  
-  // client.on('end', () => {
-  //   console.log('disconnected from server');
-  // });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
